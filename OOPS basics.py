9649108bce80ca61ca568b600793c272a69b3d08
Yingtong24 <yingtonghuo0524@gmail.com>
2025-02-28 18:47:27 +0000

    Update OOPS basics.py

--- OOPS basics.py
+++ OOPS basics.py
@@ -5,8 +5,16 @@ class person():
       self.name=name
       self.age=age
       self.country=country
+    def inp(self):
+      self.name=input("What is your name?: ")
+      self.age=input("What is your age?: ")
+      self.country=input("What country are you from?: ")
+    def out(self):
       print(self.name)
       print(self.age)
       print(self.country)
 
 obj1=person("Sophia",14,"China")
+obj1.out()
+obj1.inp()
+obj1.out()
