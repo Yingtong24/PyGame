514c5ffe6f0da488390fecdbfddacc57b9335d6d
Yingtong24 <yingtonghuo0524@gmail.com>
2025-03-21 18:22:16 +0000

    Update basicshapes.py

--- basicshapes.py
+++ basicshapes.py
@@ -7,6 +7,8 @@ blue=(199, 247, 255)
 pink=(254, 217, 255)
 purple=(221, 186, 245)
 green=(204, 255, 227)
+grey=(166, 171, 179)
+yellow=(255, 252, 92)
 screen.fill(beige)
 pygame.display.update()
 #Creating Class
@@ -20,7 +22,10 @@ class shapes:
 
 obj1=shapes(blue, (65, 65, 200,200))
 obj2=shapes(pink, (165, 165, 200, 200))
-        
+obj3=shapes(green, (265, 265, 200, 200))
+obj4=shapes(purple, (365, 365, 200, 200))
+obj5=shapes(yellow, (465, 465, 200, 200))
+obj6=shapes(grey, (565, 565, 200, 200))
 
 over=True
 while over:
@@ -29,4 +34,8 @@ while over:
             over=False
     obj1.draw()
     obj2.draw()
+    obj3.draw()
+    obj4.draw()
+    obj5.draw()
+    obj6.draw()
     pygame.display.update()
