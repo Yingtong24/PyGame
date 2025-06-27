5dcf2a1c86a862cd8ca6b39e0a55a6372ffb1162
Yingtong24 <yingtonghuo0524@gmail.com>
2025-06-27 19:09:50 +0100

    Update recycling.py

--- Recycle/recycling.py
+++ Recycle/recycling.py
@@ -5,8 +5,12 @@ from pygame.locals import *
 pygame.init()
 WIDTH=1000
 HEIGHT=1000
+st=time.time()
+score=0
 screen=pygame.display.set_mode((WIDTH, HEIGHT))
 bg=pygame.image.load("bg.jpg")
+fnt=pygame.font.SysFont("Lobster", 35)
+fnt2=pygame.font.SysFont("Caveat", 20)
 
 #Bin Class
 class bin(pygame.sprite.Sprite):
@@ -18,12 +22,71 @@ bin2=bin()
 asprite=pygame.sprite.Group()
 asprite.add(bin2)
 
+
+#Class for Recyclable items
+class recyclable(pygame.sprite.Sprite):
+    def __init__(self, img):
+        super().__init__()
+        self.image=pygame.image.load(img)
+        self.rect=self.image.get_rect()
+
+ry=["pabag.png","cc.png","pencil.png"]
+rcyble=pygame.sprite.Group()
+
+
+#Class for Non-Recyclable Items
+class nrecyclable(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image=pygame.image.load("plbag.png")
+        self.rect=self.image.get_rect()
+nrcyble=pygame.sprite.Group()
+
+#Creating recyclable and non-recyclable objects
+for i in range(30):
+    obj=recyclable(random.choice(ry))
+    obj.rect.x=random.randint(0,1000)
+    obj.rect.y=random.randint(0,1000)
+    rcyble.add(obj)
+    asprite.add(obj)
+
+for i in range(15):
+    obj=nrecyclable()
+    obj.rect.x=random.randint(0,1000)
+    obj.rect.y=random.randint(0,1000)
+    nrcyble.add(obj)
+    asprite.add(obj)
+
+
+
+
+
+
+
+
+
+
+
 run=True
 while run:
     for event in pygame.event.get():
         if event.type == QUIT:
             run=False
-    screen.blit(bg, (0, 0))
-    asprite.draw(screen)
-    pygame.display.update()
+    tt=time.time()-st
+    if tt>=30:
+        if score>=15:
+            text=fnt.render("Congrats, you win",1, "dark green")
+        else:
+            text-fnt2.render("You lost :( try again", 1, "red")
+        screen.blit(text, (500,500))
+    else:
+        screen.blit(bg, (0, 0))
+        ttxt=fnt2.render("Time Left: "+str(30-int(tt)), 1, "green")
+        screen.blit(ttxt, (100,100))
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            if bin2.rect.y>0:
+                bin2.rect.y-=5
+        asprite.draw(screen)
+        pygame.display.update()
 pygame.quit()
