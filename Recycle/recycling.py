36f2f642213be39c13703e865519ca8fd97f1894
Yingtong24 <yingtonghuo0524@gmail.com>
2025-07-04 18:44:27 +0100

    Update recycling.py

--- Recycle/recycling.py
+++ Recycle/recycling.py
@@ -10,7 +10,9 @@ score=0
 screen=pygame.display.set_mode((WIDTH, HEIGHT))
 bg=pygame.image.load("bg.jpg")
 fnt=pygame.font.SysFont("Lobster", 35)
-fnt2=pygame.font.SysFont("Caveat", 20)
+fnt2=pygame.font.SysFont("Caveat", 50)
+text=fnt2.render("Score ="+str(0), True, "black")
+
 
 #Bin Class
 class bin(pygame.sprite.Sprite):
@@ -57,16 +59,6 @@ for i in range(15):
     nrcyble.add(obj)
     asprite.add(obj)
 
-
-
-
-
-
-
-
-
-
-
 run=True
 while run:
     for event in pygame.event.get():
@@ -77,7 +69,7 @@ while run:
         if score>=15:
             text=fnt.render("Congrats, you win",1, "dark green")
         else:
-            text-fnt2.render("You lost :( try again", 1, "red")
+            text=fnt2.render("You lost :( try again", 1, "red")
         screen.blit(text, (500,500))
     else:
         screen.blit(bg, (0, 0))
@@ -87,6 +79,29 @@ while run:
         if keys[pygame.K_UP]:
             if bin2.rect.y>0:
                 bin2.rect.y-=5
+        if keys[pygame.K_LEFT]:
+            if bin2.rect.x>0:
+                bin2.rect.x-=5
+        if keys[pygame.K_RIGHT]:
+            if bin2.rect.x<1000:
+                bin2.rect.x+=5
+        if keys[pygame.K_DOWN]:
+            if bin2.rect.y<1000:
+                bin2.rect.y+=5
+#Checking Collision
+        #Recyclable Group
+        recyclehit=pygame.sprite.spritecollide(bin2, rcyble, True)
+        #Non Recyclable Group
+        nrecyclehit=pygame.sprite.spritecollide(bin2, nrcyble, True)
+#Scoring
+        for item in recyclehit:
+            score+=1
+            text=fnt2.render("Score ="+str(score), True, "black")
+        for item in nrecyclehit:
+            score-=2
+            text=fnt2.render("Score ="+str(score), True, "black")
+        screen.blit(text,(0,0))
+
         asprite.draw(screen)
         pygame.display.update()
 pygame.quit()
